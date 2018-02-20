de05dc41b803d35bed7e025a5179a0802fe414e1
NBerlin <nickiberlinpolhem@gmail.com>
2018-02-20 11:57:56 +0100

    Lab 5

---
+++ Lab 5/htdocs/app/todo.list/todo.list.component.js
@@ -0,0 +1,15 @@
+(function(app) {
+  app.TodoListComponent = TodoListComponent;
+  TodoListComponent.annotations = [
+    new ng.core.Component({
+      selector: 'todo-list',
+      templateUrl: 'app/todo.list/todo.list.component.html',
+    })
+  ];
+
+  TodoListComponent.parameters = [app.DataService];
+
+  function TodoListComponent(dataService) {
+    this.myList = dataService.getTODOs();
+  }
+})(window.app = window.app || {});
---
+++ Lab 5/htdocs/app/todo/todo.component.js
@@ -0,0 +1,21 @@
+(function(app) {
+  app.TodoComponent = TodoComponent;
+  TodoComponent.annotations = [
+    new ng.core.Component({
+      selector: 'todo',
+      templateUrl: 'app/todo/todo.component.html',
+    })
+  ];
+
+  TodoComponent.parameters = [app.DataService];
+
+  function TodoComponent(dataService) {
+    this.todo = {text: "make a list", done: false};
+
+    this.newTodo = function () {
+      dataService.addTodo({text: this.todo.text, done: this.todo.done});
+    }
+  }
+
+
+})(window.app = window.app || {});
